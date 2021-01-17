e1490f811d5a009ce7af93fe99768d37ed2986a0
WiktorWox <77573493+WiktorWox@users.noreply.github.com>
2021-01-17 15:10:29 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,4 @@
+const MinecraftAddonBuilder = require("minecraft-addon-toolchain/v1");
+
+const builder = new MinecraftAddonBuilder("KoronaŚwiat");
+module.exports = builder.configureEverythingForMe();
