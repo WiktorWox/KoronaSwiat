0920cb0764f2d06608e3fc897cb5fd44464295b0
WiktorWox <77573493+WiktorWox@users.noreply.github.com>
2021-07-28 00:10:11 +0200

    Updated scripts
    Co-Authored-By: Daniel Boczek <659399+dboczek@users.noreply.github.com>

--- packs/scripts/scripts/server/serverScript.js
+++ packs/scripts/scripts/server/serverScript.js
@@ -3,14 +3,11 @@ var system = systemServer;
 
 var tickNumber = 0;
 
+var playersData = {
+}
+
 
 systemServer.initialize = function() {
-	systemServer.listenForEvent("minecraft:entity_equipped_armor", function(e) {
-	 // if ("korona:soul_chestplate" == e.item_stack) {
-	  		systemServer.log(e)
-		// }
-	});
-
 	const scriptLoggerConfig = this.createEventData(
 		'minecraft:script_logger_config'
 	)
@@ -18,10 +15,36 @@ systemServer.initialize = function() {
 	scriptLoggerConfig.data.log_information = true
 	scriptLoggerConfig.data.log_warnings = true
 	this.broadcastEvent('minecraft:script_logger_config', scriptLoggerConfig)
+	systemServer.log("initialize started")
+  	// this.listenForEvent("minecraft:client_entered_world", (eventData) => this.onClientEnteredWorld(eventData));
+
+	this.listenForEvent("minecraft:entity_equipped_armor", function(e) {
+	 // if ("korona:soul_chestplate" == e.item_stack) {
+	  		// if (!e.data.entity.id in playersData){
+	  		// 	playersData[e.data.entity.id] = {
+	  		// 		"soulCheastPlate": false
+	  		// 	}
+	  		// }
+  			// let playerData = playersData[e.data.entity.id]
+  			// if (ubrał co){
+  		system.log(e)
+  			// }
+
+		// }
+		let myQuery = system.registerQuery()
+		// system.addFilterToQuery(myQuery, "minecraft:name");
+		let players = system.getEntitiesFromQuery(myQuery);
+		for(var entity in players) {
+			system.log(entity)
+  		}
+	});
+
+
 
 	// register event data, register components, register queries, listen for events, . . .
 
 	this.counter = 0
+	systemServer.log("initialize finished")
 }
 
 // the server runs this update function 20 times per second
@@ -74,3 +97,10 @@ systemServer.log = function (...items) {
 	chatEvent.data.message = items.map(toString).join(' ')
 	this.broadcastEvent('minecraft:display_chat_event', chatEvent)
 }
+
+
+
+system.onClientEnteredWorld = function(eventData) {
+	system.log('entered')
+	system.log(eventData)
+};
