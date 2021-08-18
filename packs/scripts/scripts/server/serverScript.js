3f1ac131daaa2db56ce294b10965018a62da8f0e
WiktorWox <77573493+WiktorWox@users.noreply.github.com>
2021-08-18 17:51:30 +0200

    Update serverScript.js

--- packs/scripts/scripts/server/serverScript.js
+++ packs/scripts/scripts/server/serverScript.js
@@ -6,7 +6,7 @@ var tickNumber = 0;
 var playersData = {
 }
 
-
+let commandData
 systemServer.initialize = function() {
 	const scriptLoggerConfig = this.createEventData(
 		'minecraft:script_logger_config'
@@ -19,20 +19,55 @@ systemServer.initialize = function() {
   	// this.listenForEvent("minecraft:client_entered_world", (eventData) => this.onClientEnteredWorld(eventData));
 
 	this.listenForEvent("minecraft:entity_equipped_armor", function(e) {
-  		system.log(e)
-  		let armor = e.item_stack.item
+		system.log(e)
+		let armorSlot = e.data.slot
+  		let somethingHappend
+  		let armor = e.data.item_stack.item
+  		let playerId = e.data.entity
+  		let nameComponent = system.getComponent(playerId, "minecraft:nameable");
+  		let playerName = nameComponent.data.name
   		switch (armor) {
   		    case "korona:soul_chestplate":
-  		    	system.log("Założono zbroję dusz")
-  				if (!e.data.entity.id in playersData){
-	  	 			playersData[e.data.entity.id] = {
-	  	 				"soulChestPlateEquipped": false
-	  	 			}
+  		   		if (!(playerId in playersData)) {
+		  			playersData[playerId] = {
+			  	 		"soulChestPlateEquipped": false,
+			  	 		"soulBootsEquipped": false,
+			  	 		"soulHelmetEquipped": false,
+			  	 		"soulLeggingsEquipped": false,
+			  	 		"fullSoulArmorEquipped": false,
+			  	 		"playerName": 
+			  	 	}
+			  	 	system.log(playersData[playerId])
+
+		  		}
+  		    	system.log("Założono napierśnik dusz")
+	  	 		playersData[playerId].soulChestPlateEquipped = true
+
+    			system.log("Gracz który założył zbroję nazywa się " + playerName)
+
+	  	 		somethingHappend = true
+	  	 		break;
+	  	 	case "minecraft:undefined":
+	  	 		if (playersData[playerId].soulChestPlateEquipped == true && armorSlot = "slot.armor.chest") {
+	  	 			playersData[playerId].soulChestPlateEquipped = false
+	  	 			system.log("Zdjęto")
 	  	 		}
+	  	 		somethingHappend = true
+	  	 		break
   		}
+	  	if (somethingHappend == true) {
+	  		if (playersData[playerId].soulChestPlateEquipped == false && playersData[playerId].soulLeggingsEquipped == false && playersData[playerId].soulHelmetEquipped == false && playersData[playerId].soulBootsEquipped == false) {
+		  	 	system.log("Zdjęto zbroję dusz")
+		  	 	playersData[playerId].fullSoulArmorEquipped = false
+		  	 	somethingHappend = undefined
+		  	}
+		  	if (playersData[playerId].soulChestPlateEquipped == true && playersData[playerId].soulLeggingsEquipped == true && playersData[playerId].soulHelmetEquipped == true && playersData[playerId].soulBootsEquipped == true) {
+		  	 	system.log("Założono zbroję dusz")
+		  	 	playersData[playerId].fullSoulArmorEquipped = true
+		  	 	somethingHappend = undefined
+		 	}
+	  	}
 	});
-
-
 
 	// register event data, register components, register queries, listen for events, . . .
 
@@ -42,15 +77,20 @@ systemServer.initialize = function() {
 
 // the server runs this update function 20 times per second
 systemServer.update = function () {
-	// print hello world to the world's chat once per second
-	// this.counter++
-	// if (this.counter === 20) {
-	// 	this.log('Server!')
-	// 	this.counter = 0
-	// }
+	this.counter++
+	if (this.counter === 100) {
+		if ()
+			commandData.soulArmorSpeed = {
+		  	 	"__type__" : "event_data",
+		  	 	"data" : {
+		  	 		"command" : "effect " + playerName + " speed 5 1"
+		  	 	},
+		  	"__identifier__" : "minecraft:execute_command"
+		  	}
 
- //    // increase the tick number
- //    tickNumber++;
+	    	system.broadcastEvent("minecraft:execute_command", commandData.soulArmorSpeed)
+		this.counter = 0
+	}
 
 }
 
